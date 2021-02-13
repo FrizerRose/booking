fa57e8f9a110486bd5f7b9d28ecd51370978d38c
JurajMarkesic <jmarkesic@tvz.hr>
2021-02-13 18:22:03 +0100

    Created Card component for services.

--- src/components/bookingSteps/BookingStepServices.vue
+++ src/components/bookingSteps/BookingStepServices.vue
@@ -8,41 +8,11 @@
   >
     <!-- usluge -->
     <ul class="o-layout o-list XXXXXXXX-gutter">
-      <li
+      <BookingStepServiceCard
         v-for="service in services"
         :key="service.id"
-        class="o-layout_item o-list_item -gutters-px-10 || XXXXXXXXo-flex -flex-column -justify-center -align-center"
-      >
-        <div class="o-background-wrap">
-          <div class="o-background -has-shadow" />
-          <article class="c-card-service has-links-inside">
-            <div class="o-layout">
-              <div class="o-layout_item u-1/3@to-medium u-1/4@from-medium">
-                <figure class="c-card-service_image-wrap -max-width || o-ratio">
-                  <img
-                    class="c-card-service_image"
-                    src="https://www.mdstrat.com/wp-content/uploads/2020/07/payment_integrity_facs.svg"
-                    alt="img text"
-                  >
-                </figure>
-              </div>
-              <div class="o-layout_item u-2/3@to-medium u-3/4@from-medium">
-                <div class="c-card-service_info">
-                  <h1 class="c-heading">
-                    {{ service.name }}
-                  </h1>
-                  <button
-                    class="c-button -primary || has-links-inside_main-link"
-                    @click="nextStep()"
-                  >
-                    <span class="c-button_label">cta</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </article>
-        </div>
-      </li>
+        :service="service"
+      />
     </ul>
     <!-- ends usluge -->
   </li>
@@ -51,21 +21,18 @@
 <script lang='ts'>
 import { defineComponent, computed } from 'vue';
 import { useStore } from '@/store';
-import MutationTypes from '@/store/mutation-types';
+import BookingStepServiceCard from './BookingStepServiceCard.vue';
 
 export default defineComponent({
+  components: {
+    BookingStepServiceCard,
+  },
   setup() {
     const store = useStore();
-    const currentStep = computed(() => store.state.shared.currentStep);
-
-    function nextStep() {
-      store.commit(MutationTypes.CHANGE_CURRENT_STEP, currentStep.value + 1);
-    }
 
     const services = computed(() => store.state.service.services);
 
     return {
-      nextStep,
       services,
     };
   },
