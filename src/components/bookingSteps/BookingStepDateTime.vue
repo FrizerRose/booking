6822c5e5448a6a22a7e98da03339bc481cbef71b
JurajMarkesic <jmarkesic@tvz.hr>
2021-02-14 10:51:58 +0100

    Added array of possible appointment times.

--- src/components/bookingSteps/BookingStepDateTime.vue
+++ src/components/bookingSteps/BookingStepDateTime.vue
@@ -16,9 +16,9 @@
           id="start"
           type="date"
           name="trip-start"
-          value="2018-07-22"
-          min="2018-01-01"
-          max="2018-12-31"
+          value="2021-02-14"
+          min="2021-02-14"
+          max="2021-03-14"
         >
       </div>
     </div>
@@ -27,268 +27,16 @@
         class="o-layout_item u-1/2@from-medium -gutters-px-10 || XXXXXXXXo-flex -flex-column -justify-center -align-center"
       >
         <ul class="c-timeslots-list o-layout -gutter || o-list">
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
+          <li
+            v-for="time in possibleAppointmentTimes"
+            :key="time"
+            class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item"
+          >
             <button
               class="c-button -outline"
-              @click="nextStep()"
+              @click="nextStep(time)"
             >
-              <span class="c-button_label">8:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">8:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">8:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">8:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">9:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">9:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">9:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">9:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">10:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">10:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">10:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">10:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">11:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">11:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">11:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">11:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">12:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">12:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">12:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">12:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">13:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">13:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">13:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">13:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">14:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">14:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">14:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">14:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">15:00</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">15:15</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">15:30</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">15:45</span>
-            </button>
-          </li>
-          <li class="c-timeslots-list_item o-layout_item u-1/2 || o-list_item">
-            <button
-              class="c-button -outline"
-              @click="nextStep()"
-            >
-              <span class="c-button_label">16:00</span>
+              <span class="c-button_label">{{ time }}</span>
             </button>
           </li>
         </ul>
@@ -308,12 +56,24 @@ export default defineComponent({
     const store = useStore();
     const currentStep = computed(() => store.state.shared.currentStep);
 
-    function nextStep() {
+    function nextStep(time: string) {
       store.commit(MutationTypes.CHANGE_CURRENT_STEP, currentStep.value + 1);
+    }
+
+    // TODO: fetch startTime and endTime from the api
+    const startTime = 8;
+    const endTime = 16;
+    const possibleAppointmentTimes = [];
+    for (let i = startTime; i < endTime; i += 1) {
+      possibleAppointmentTimes.push(`${i}:00`);
+      possibleAppointmentTimes.push(`${i}:15`);
+      possibleAppointmentTimes.push(`${i}:30`);
+      possibleAppointmentTimes.push(`${i}:45`);
     }
 
     return {
       nextStep,
+      possibleAppointmentTimes,
     };
   },
 });
---
+++ src/store/modules/appointment/actions.ts
@@ -0,0 +1,61 @@
+// Cyclical dependency needed to make vuex fully typed.
+// eslint-disable-next-line import/no-cycle
+import { RootState } from '@/store';
+import { ActionContext, ActionTree } from 'vuex';
+import { CompanyService } from '@/api';
+import { ApiError } from '@/types/customError';
+import LocalActionTypes from './action-types';
+import ServiceMutationTypes from '../service/mutation-types';
+import StaffMutationTypes from '../staff/mutation-types';
+import LocalMutationTypes from './mutation-types';
+import { Mutations } from './mutations';
+import { Mutations as ServiceMutations } from '../service/mutations';
+import { Mutations as StaffMutations } from '../staff/mutations';
+import { State } from './state';
+
+// Constraints commit to mutations from the right module
+type AugmentedActionContext = {
+  commit<K extends keyof Mutations>(
+    key: K,
+    payload: Parameters<Mutations[K]>[1],
+  ): ReturnType<Mutations[K]>;
+} & Omit<ActionContext<State, RootState>, 'commit'>
+
+type AugmentedServiceActionContext = {
+  commit<K extends keyof ServiceMutations>(
+    key: K,
+    payload: Parameters<ServiceMutations[K]>[1],
+  ): ReturnType<ServiceMutations[K]>;
+} & Omit<ActionContext<State, RootState>, 'commit'>
+
+type AugmentedStaffActionContext = {
+  commit<K extends keyof StaffMutations>(
+    key: K,
+    payload: Parameters<StaffMutations[K]>[1],
+  ): ReturnType<StaffMutations[K]>;
+} & Omit<ActionContext<State, RootState>, 'commit'>
+
+export interface Actions {
+  [LocalActionTypes.FETCH_COMPANY](
+    { commit }: AugmentedActionContext & AugmentedServiceActionContext & AugmentedStaffActionContext,
+    id: number
+  ): void;
+}
+
+// API access.
+const companyService = new CompanyService();
+
+// Action implementation.
+export const actions: ActionTree<State, RootState> & Actions = {
+  async [LocalActionTypes.FETCH_COMPANY]({ commit }, id: number) {
+    const response = await companyService.get(id);
+    console.log(response, response.data);
+    if (response.status === 200 && response.data !== undefined) {
+      commit(LocalMutationTypes.CHANGE_COMPANY, response.data);
+      commit(ServiceMutationTypes.CHANGE_SERVICES, response.data.services);
+      commit(StaffMutationTypes.CHANGE_STAFF, response.data.staff);
+    } else {
+      throw new ApiError('No company by this ID.');
+    }
+  },
+};
